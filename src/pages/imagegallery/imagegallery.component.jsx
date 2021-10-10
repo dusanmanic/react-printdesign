5a302296ea06c27ee5501b9e3d0e7b7a23e7cd52
dusanmanic <dusan.manic@hotmail.com>
2021-10-10 18:45:41 +0200

    IntersectionObserver

--- src/pages/imagegallery/imagegallery.component.jsx
+++ src/pages/imagegallery/imagegallery.component.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useContext, useRef} from 'react'
+import React, {useState, useEffect, useContext, createRef} from 'react'
 
 import { useHistory } from 'react-router-dom'
 import { Context } from '../../context/context'
@@ -8,56 +8,60 @@ import './imagegallery.styles.css'
 
 export default function ImageGallery({collectionName}) {
 
-    const componentRef = useRef(null);
-
-    let defaultLoadNumber
-
-    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        defaultLoadNumber = Math.floor(window.innerHeight / 100) * Math.floor(window.innerWidth / 160)
-    } else {
-        defaultLoadNumber = Math.floor(window.innerHeight / 160 * 3) // 100px height slike, 5 slika ima u redu
-    }
-
-    //(window.innerHeight + window.scrollY) >= document.documentElement.offsetHeight // Provera da li je scroll dosao do kraja
-
     const history = useHistory()
-    const randomToken = Math.random().toString(36).substr(2)  
     
     const {galleryName, fullScreen, clickedImage} = useContext(Context)
     const [currentGalleryUrls, setCurrentGalleryUrls] = fullScreen
     const [galeryName, setGaleryName] = galleryName
     const [clickedImg, setClickedImg] = clickedImage
-    const [loadImagesLazyLoading, setLoadImagesLazyLoading] = useState(defaultLoadNumber)
     const [currentGallery, setCurrentGallery] = useState([])
 
     const FullScreenImage = event => {
         setClickedImg(event.target.style.backgroundImage.substring(5).slice(0, -2)) //.substrin(4) brise prva 4, .slice(0, -1) zadnji
         setGaleryName(event.target.id)
         history.push(`/fullscreen/${event.target.id}`)
-    }    
+    }   
+    
+    let options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.3
+    }
+
+    let callback = (entries) => {
+        // console.log(entries)
+        entries.forEach(( entrie, index) => {
+            // console.log(entrie.isIntersecting)
+            if(entrie.isIntersecting && entrie.isIntersecting !== "undefined") {
+                console.log(entrie.target.className.slice(9))
+                // console.log(currentGallery[index].downLink)
+                entrie.target.style.backgroundImage = `url("${currentGallery[entrie.target.className.slice(9)].downLink}")`
+                
+            }
+        })
+    }
 
     useEffect(() => {
-        document.addEventListener("scroll", () => {
-                
-            if((window.innerHeight + window.scrollY) >= document.documentElement.offsetHeight) {
-                setLoadImagesLazyLoading(loadImagesLazyLoading + defaultLoadNumber)
-            }    
+        let observer = new IntersectionObserver(callback, options)
+        // let target = componentRef.current
+        // if(target) observer.observe(target)
+        refs.map( (ref, index) => {
+            // console.log(ref)
+            let target = ref.current
+            if(target) observer.observe(target)
         })
-    }) 
+    })
     
     useEffect(() => {        
         let currentGalleryArray = []
-        let currentGalleryArrayUrls = []
-
         firestore.collection(`${collectionName}`)
         .get()
         .then(querySnapshot => {
             let allDocs = querySnapshot.docs
             allDocs.forEach( (doc, index) => {
                 let data = doc.data()
-                console.log(data)
-                currentGalleryArray.push(data)
-                currentGalleryArrayUrls.push(data.downLink)                
+                // console.log(data)
+                currentGalleryArray.push(data)               
             })            
         })
         .then(() => {
@@ -69,26 +73,25 @@ export default function ImageGallery({collectionName}) {
               });
             console.log(currentGalleryArray)
             setCurrentGallery(currentGalleryArray)
-            setCurrentGalleryUrls(currentGalleryArrayUrls)
         })
     }, [])
 
+    const refs = currentGallery.map(() => createRef())
+
     return(
-        <div className="imagegallery-page-wrapper" ref={componentRef}>
-            <div className="gallery-wrapper" >
-                {console.log(loadImagesLazyLoading)}
+        <div className="imagegallery-page-wrapper">
+            <div className="gallery-wrapper">
+                {console.log(currentGallery)}
+                {/* {console.log(refs)} */}
                 {
                     currentGallery.map((data, index) => {
-                        if(loadImagesLazyLoading >= index) {
-                            return (
-                                <div
-                                key={index}
-                                id={randomToken}
-                                className={"slikaImg"}
-                                style={{backgroundImage: `url("${data.downLink}")`}} 
-                                onClick={FullScreenImage} />                                
-                            )
-                        }                        
+                        return (
+                            <div
+                            className={`slikaImg ${index}`}
+                            ref={refs[index]}
+                            key={index}
+                            onClick={FullScreenImage}/>                             
+                        )
                     })
                 }
             </div>
---
+++ src/pages/imagegallery_backup/imagegallery.component.jsx
@@ -0,0 +1,96 @@
+import React, {useState, useEffect, useContext, useRef} from 'react'
+
+import { useHistory } from 'react-router-dom'
+import { Context } from '../../context/context'
+import { firestore } from '../../firebase/firebase.utils'
+
+import './imagegallery.styles.css'
+
+export default function ImageGallery({collectionName}) {
+
+    const componentRef = useRef(null);
+
+    let defaultLoadNumber
+
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        defaultLoadNumber = Math.floor(window.innerHeight / 100) * Math.floor(window.innerWidth / 160)
+    } else {
+        defaultLoadNumber = Math.floor(window.innerHeight / 160 * 3) // 100px height slike, 5 slika ima u redu
+    }
+
+    //(window.innerHeight + window.scrollY) >= document.documentElement.offsetHeight // Provera da li je scroll dosao do kraja
+
+    const history = useHistory()
+    const randomToken = Math.random().toString(36).substr(2)  
+    
+    const {galleryName, fullScreen, clickedImage} = useContext(Context)
+    const [currentGalleryUrls, setCurrentGalleryUrls] = fullScreen
+    const [galeryName, setGaleryName] = galleryName
+    const [clickedImg, setClickedImg] = clickedImage
+    const [loadImagesLazyLoading, setLoadImagesLazyLoading] = useState(defaultLoadNumber)
+    const [currentGallery, setCurrentGallery] = useState([])
+
+    const FullScreenImage = event => {
+        setClickedImg(event.target.style.backgroundImage.substring(5).slice(0, -2)) //.substrin(4) brise prva 4, .slice(0, -1) zadnji
+        setGaleryName(event.target.id)
+        history.push(`/fullscreen/${event.target.id}`)
+    }    
+
+    // useEffect(() => {
+    //     document.addEventListener("scroll", () => {                
+    //         if((window.innerHeight + window.scrollY) >= document.documentElement.offsetHeight) {
+    //             setLoadImagesLazyLoading(loadImagesLazyLoading + defaultLoadNumber)
+    //         }    
+    //     })
+    // }) 
+    
+    useEffect(() => {        
+        let currentGalleryArray = []
+        let currentGalleryArrayUrls = []
+
+        firestore.collection(`${collectionName}`)
+        .get()
+        .then(querySnapshot => {
+            let allDocs = querySnapshot.docs
+            allDocs.forEach( (doc, index) => {
+                let data = doc.data()
+                console.log(data)
+                currentGalleryArray.push(data)
+                currentGalleryArrayUrls.push(data.downLink)                
+            })            
+        })
+        .then(() => {
+            console.log(currentGalleryArray)
+            currentGalleryArray.sort(function(a, b){
+                // Turn your strings into dates, and then subtract them
+                // to get a value that is either negative, positive, or zero.
+                return b.datum - a.datum
+              });
+            console.log(currentGalleryArray)
+            setCurrentGallery(currentGalleryArray)
+            setCurrentGalleryUrls(currentGalleryArrayUrls)
+        })
+    }, [])
+
+    return(
+        <div className="imagegallery-page-wrapper" ref={componentRef}>
+            <div className="gallery-wrapper" >
+                {console.log(loadImagesLazyLoading)}
+                {
+                    currentGallery.map((data, index) => {
+                        if(loadImagesLazyLoading >= index) {
+                            return (
+                                <div
+                                key={index}
+                                id={randomToken}
+                                className={"slikaImg"}
+                                style={{backgroundImage: `url("${data.downLink}")`}} 
+                                onClick={FullScreenImage} />                                
+                            )
+                        }                        
+                    })
+                }
+            </div>
+        </div>
+    )
+}
